5962c64b8d9d52951933a3ebff729ba95ad5d5d8
samraeng temram <elect_2510@hotmail.com>
2019-04-14 14:32:42 +0700

    Reactive playing

--- main_module.py
+++ main_module.py
@@ -1,5 +1,6 @@
 import rx
 from rx import Observable, Observer
+import time
 
 class MyObserver(Observer):
   def on_next(self, x):
@@ -13,7 +14,34 @@ class MyObserver(Observer):
 
 
 if __name__ == "__main__":
-  xs = Observable.from_iterable(["red", "green", "orange"])
+  xs = Observable.from_iterable(range(10)).publish()
   d = xs \
-      .map( lambda x: "process %a" % x ) \
+      .map(lambda x: "Correct" if x == 45 else "In Correct") \
       .subscribe(MyObserver())
+
+  # e = xs \
+  #     .map(lambda x: "non-delayed %s" % x) \
+  #     .subscribe(MyObserver())
+
+  powerEvent = Observable.from_callback()
+
+  it = Observable.interval(1000).publish()
+
+  processedXs = xs \
+    .map(lambda x: x * x) \
+    .filter(lambda x: x > 30) \
+    .reduce(lambda x, y: x + y, 0)
+
+  throttledSequences = processedXs.zip(it, lambda asx, ait: asx) \
+      .map(lambda x: "Throttled %s" % x) \
+      .subscribe(MyObserver())
+
+  xs.connect()
+  it.connect()
+
+  print("before sleep")
+  time.sleep(10)
+  print("finished execution")
+
+
+  
