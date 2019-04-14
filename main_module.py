73b6d1e97980fc922f1223099df162199f748361
samraeng temram <elect_2510@hotmail.com>
2019-04-14 15:54:08 +0700

    Add event experiments

--- main_module.py
+++ main_module.py
@@ -14,17 +14,21 @@ class MyObserver(Observer):
 
 class CoinDispenser(Observer):
   def on_next(self, coin):
-    print("dispense coin %s" % coin)
+    self.ignoreNewCoin(True)
 
   def on_error(self, error):
     print("error")
 
   def on_completed(self):
     print("completed")
+
+  def ignoreNewCoin(self, ignore):
+    pass
+
 
 class CoinLCDOutputObserver(Observer):
   def on_next(self, coin):
-    print("display LCD %s" % coin)
+    self.outputLCD(coin)
 
   def on_error(self, error):
     print("error")
@@ -32,26 +36,66 @@ class CoinLCDOutputObserver(Observer):
   def on_completed(self):
     print("completed")
 
-if __name__ == "__main__":
+  def outputLCD(self, digits):
+    print("Implement here")
 
-  def handleInterrupt(coin):
-    print("produce coin event")
+
+
+class StartMachine(Observer):
+  def on_next(self, coin):
+    print("StartMachine %s" % coin)
+
+  def on_error(self, error):
+    print("error")
+
+  def on_completed(self):
+    print("completed")
+
+class WaterLevelController(Observer):
+  def on_next(self, coin):
+    self.pushButton()
+
+  def on_error(self, error):
+    print("error")
+
+  def on_completed(self):
+    print("completed")
+
+  def pushButton(self):
+    pass
+
+
+    
+
+if __name__ == "__main__":
 
   coinEvents = Observable.from_list([5, 10, 5, 5, 10, 5]).publish()
 
 
+
+  Observable.from_iterable(range(4)) \
+    .zip(Observable.interval(250)) \
+    .subscribe(WaterLevelController())
+
   totalCoinEvent = Observable.from_iterable(range(6)) \
-    .flat_map(lambda r: coinEvents.take(r).reduce(lambda a, b: a + b, 0)) \
+    .flat_map(lambda r: coinEvents.take(r) \
+      .reduce(lambda a, b: a + b, 0)
+    )
 
   totalCoinEvent.subscribe(CoinLCDOutputObserver())
 
   totalCoinEvent \
     .filter(lambda x: x > 20) \
+    .map(lambda x: True) \
     .subscribe(CoinDispenser())
 
   totalCoinEvent \
     .filter(lambda x: x == 20) \
     .subscribe(StartMachine())
+
+    
+  coinEvents.subscribe(on_next=lambda x: print("This is coin event %s" % x))
+  totalCoinEvent.subscribe(on_next=lambda x: print("This is total coin event %s" % x))
 
   coinEvents.connect()
 
